500a175fd3ea8f98bcdf2753d16c05435fc13b2b
Andrew <svt.andrey1631@gmail.com>
2020-03-07 00:35:18 +0200

    Создал проект

---
+++ proga_laba_2/proga_laba_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	ifstream file1("eurovision1.csv");
+	ifstream file2("eurovision2.csv");
+	int n1, n2;  //колличество элементов в файле
+	file1 >> n1; file2 >> n2;
+	string* s1 = new string[n1];
+	string* s2 = new string[n2];
+}
+
