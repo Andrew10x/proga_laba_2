5fa6fe7356eecdc978635d2ea1ac7a032f0add4b
Andrew <svt.andrey1631@gmail.com>
2020-03-08 16:17:05 +0200

    Разбил имя страны и сумму баллов по массивам

--- proga_laba_2/proga_laba_2.cpp
+++ proga_laba_2/proga_laba_2.cpp
@@ -4,6 +4,7 @@
 #include <string>
 using namespace std;
 void from_file(string*, ifstream&, ifstream&, int, int);  //считывание из файла
+void fill(string*, string*, long int*, int); //заполнение массивов считанными данными
 
 
 int main()
@@ -17,6 +18,12 @@ int main()
 	(file1 >> n1).get(); (file2 >> n2).get();
 	string* s = new string[n1+n2];
 	from_file(s, file1, file2, n1, n2);
+	
+	int n = n1 + n2;
+	string* name = new string[n];
+	long int* marks = new long int[n];
+	fill(s, name, marks, n);
+	
 	}
 	else cout << "Файл не открыт";
 	
@@ -29,5 +36,28 @@ void from_file(string* s, ifstream& file1, ifstream& file2, int n1, int n2)
 
 	for (int i = n1; i < n1 + n2; i++)
 		getline(file2, s[i]); 
+	file1.close(); file2.close();
+}
+
+void fill(string* s, string* name, long int* marks, int n)
+{
+	
+	for (int i = 0; i < n; i++)
+	{
+		size_t p1, p2 = 0;
+		s[i].push_back(',');
+		p1 = s[i].find(',', p2);
+		name[i] = s[i].substr(p2, p1-p2);
+		p2 = p1 + 1; p1 = s[i].find(',', p2);
+
+		long int sum = 0;
+		while(p1!= string ::npos)
+		{	
+			sum += stol(s[i].substr(p2, p1-p2),0,10);
+			p2 = p1 + 1;
+			p1 = s[i].find(',', p2);	
+		}
+		marks[i] = sum;
+	}
 }
 
